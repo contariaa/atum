e424e8a4721d636d6eb4bf1282bed0c00e4a4d49
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2025-05-05 10:01:54 -0300

    Major SeedProvider Refactor + Explicit AtumCreateWorldScreen Job

--- src/main/java/me/voidxwalker/autoreset/AtumCreateWorldScreen.java
+++ src/main/java/me/voidxwalker/autoreset/AtumCreateWorldScreen.java
@@ -13,13 +13,28 @@ import java.util.Optional;
 import java.util.OptionalLong;
 
 public class AtumCreateWorldScreen extends CreateWorldScreen {
+    private final Job job;
 
-    private AtumCreateWorldScreen(@Nullable Screen parent, DataPackSettings dataPackSettings, MoreOptionsDialog moreOptionsDialog) {
+    private AtumCreateWorldScreen(@Nullable Screen parent, DataPackSettings dataPackSettings, MoreOptionsDialog moreOptionsDialog, Job job) {
         super(parent, dataPackSettings, moreOptionsDialog);
+        this.job = job;
     }
 
     public static AtumCreateWorldScreen create(@Nullable Screen parent) {
+        return create(parent, Job.CREATION);
+    }
+
+    public static AtumCreateWorldScreen create(@Nullable Screen parent, Job job) {
         DynamicRegistryManager.Immutable registryManager = DynamicRegistryManager.BUILTIN.get();
-        return new AtumCreateWorldScreen(parent, DataPackSettings.SAFE_MODE, new MoreOptionsDialog(registryManager, GeneratorOptions.getDefaultOptions(registryManager), Optional.of(GeneratorType.DEFAULT), OptionalLong.empty()));
+        return new AtumCreateWorldScreen(parent, DataPackSettings.SAFE_MODE, new MoreOptionsDialog(registryManager, GeneratorOptions.getDefaultOptions(registryManager), Optional.of(GeneratorType.DEFAULT), OptionalLong.empty()), job);
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public enum Job {
+        CREATION,
+        CONFIGURATION
     }
 }
